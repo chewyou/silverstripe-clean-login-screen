17a016855ed44a9312f7b8c000917e92a8fd1a6d
ben.pickett <ben.pickett@deviate.net.nz>
2019-09-24 16:24:19 +1200

    Updates

--- client/src/js/main.js
+++
@@ -1,9 +0,0 @@
-import tooltip from './components/tooltip';
-
-tooltip();
-
-var form = document.getElementById('MemberLoginForm_LoginForm')
-
-form.addEventListener('submit', function(e) {
-  form.className += " loading";
-})
